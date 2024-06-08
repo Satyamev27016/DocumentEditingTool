66db8444f2fad612534b308016c1b19f4058db08
Satyamev Singh <135261227+Satyamev27016@users.noreply.github.com>
2024-06-08 11:19:04 +0530

    Update db.js
    username and pasword change

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const Connection = async (username = 'satyamev22169014', password = 'Satyamev7016' ) => {
+const Connection = async (username = '******', password = '*******' ) => {
     const URL = `mongodb+srv://${username}:${password}@documenteditor.7boehrn.mongodb.net/?retryWrites=true&w=majority`
 
 
